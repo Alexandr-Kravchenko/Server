82fa5fe34acd426b0823ebb62064622961b594c5
Alexandr-Kravchenko <sanya5478@gmail.com>
2021-08-09 12:30:46 +0300

    added dashboard

--- TodolistDB(knexjs)/routes/index.js
+++ TodolistDB(knexjs)/routes/index.js
@@ -3,8 +3,10 @@ const router = express.Router();
 
 import lists from './lists/lists.js';
 import collection from './collection.js';
+import dashboard from './dashboard.js';
+
 
 router.use('/lists', lists);
-router.use('/collection', collection);
+router.use('/dashboard', dashboard);
 
 export default router
